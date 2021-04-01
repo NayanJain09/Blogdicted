fb493b8f74f811f6706f86669564d8b54100a5ef
NayanJain <nayanjain2120@gmail.com>
2021-04-02 00:07:33 +0530

    settings.ini

--- blogdicted/settings.py
+++ blogdicted/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_USER = config('EMAIL_USER',default='')
 # EMAIL_HOST_PASSWORD =
 EMAIL_PORT = config('EMAIL_PORT',cast=int)
 
