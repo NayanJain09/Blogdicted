8191af2a2b7110f25c45255ce58e987051c9f6ec
Nayan Jain <nayanjain2120@gmail.com>
2021-07-20 13:37:10 +0530

    removed security key

--- blogdicted/settings.py
+++ blogdicted/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = '@j5^_--mr570g$@z%*fgfi97f7s)^rv6f$8d#9+3ap3)oi15e-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -146,9 +146,9 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = ''
 # EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_USER = "nayankala2120@gmail.com"
+EMAIL_HOST_USER = ""
 # EMAIL_HOST_PASSWORD =
 # EMAIL_PORT = config('EMAIL_PORT',cast=int)
 EMAIL_PORT= 587
